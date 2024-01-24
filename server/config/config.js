79859048ff3b052982495fc6cd4925f29ee9eb30
Moez SOMAI <somaimoez92@gmail.com>
2024-01-24 11:19:50 +0100

    connect Redis

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,6 @@ module.exports = {
         url: 'mongodb://localhost/wallet',
     },
     redis: {
-        host: 'localhost',
-        port: 6379,
+        url: 'redis://127.0.0.1:6379',
     },
 };
--- server/server.js
+++ server/server.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const redis = require('redis');
 const bodyParser = require('body-parser');
 const credentialRoutes = require('./routes/credetialRoutes');
 const config = require('./config/config');
 
 const app = express();
 
-mongoose.connect(config.mongo.url, { useNewUrlParser: true, useUnifiedTopology: true });
+// Connexion to Mongo
+mongoose.connect(config.mongo.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB with error - ', err));
+
+// Connect to Redis
+const redisClient = redis.createClient({url:config.redis.url});
+redisClient.connect()
+.then(() => console.log('Connected to Redis'))
+.catch((err) => console.error('Failed to connect to Redis with error - ', err));
+
 
 app.use(bodyParser.json());
 app.use('/api', credentialRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
