e71c6cc1a6f95050d49b95ca0280a901f7c5e8c7
Moez SOMAI <somaimoez92@gmail.com>
2024-01-24 11:38:42 +0100

    refacto server

--- server/server.js
+++ server/server.js
@@ -4,32 +4,24 @@ const redis = require('redis');
 const bodyParser = require('body-parser');
 const credentialRoutes = require('./routes/credetialRoutes');
 const config = require('./config/config');
+const { connectToMongo, connectToRedis } = require('./utils');
 
-const app = express();
+initialize();
 
-// Connexion to Mongo
-mongoose.connect(config.mongo.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Failed to connect to MongoDB with error - ', err));
+async function initialize() {
+  await connectToMongo();
+  await connectToRedis();
 
-// Connect to Redis
-const redisClient = redis.createClient({url:config.redis.url});
-redisClient.connect()
-.then(() => console.log('Connected to Redis'))
-.catch((err) => console.error('Failed to connect to Redis with error - ', err));
-
-
-app.use(bodyParser.json());
-app.use('/api', credentialRoutes);
-
-app.use((req, res, next) => {
-  res.status(404).send('Not Found');
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  const app = express();
+  app.use(bodyParser.json());
+  app.use('/api', credentialRoutes);
+  
+  app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+  });
+  
+  const PORT = process.env.PORT || 3001;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
---
+++ server/utils.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const config = require('./config/config');
+
+async function connectToMongo() {
+  try {
+    await mongoose.connect(config.mongo.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Failed to connect to MongoDB with error - ', err);
+  }
+}
+
+async function connectToRedis() {
+  const redisClient = redis.createClient({ url: config.redis.url });
+
+  try {
+    await new Promise((resolve, reject) => {
+      redisClient.on('connect', resolve);
+      redisClient.on('error', (err) => reject(err));
+    });
+
+    console.log('Connected to Redis');
+  } catch (err) {
+    console.error('Failed to connect to Redis with error - ', err);
+  }
+}
+
+module.exports = {
+  connectToMongo,
+  connectToRedis,
+};
